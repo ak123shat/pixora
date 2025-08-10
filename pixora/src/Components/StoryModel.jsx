56e9a8ead5eace1d6a5536c77b4ce10ded0f4006
ak123shat <jainakshat979@gmail.com>
2025-08-10 20:04:09 +0100

    frontend almost completed

--- pixora/src/Components/StoryModel.jsx
+++ pixora/src/Components/StoryModel.jsx
@@ -1,9 +1,11 @@
+import { ArrowLeft, Sparkle, TextIcon, Upload } from 'lucide-react';
 import React from 'react'
 import { useState } from 'react'
+import toast from 'react-hot-toast';
 
 
 
-const StoryModel = () => {
+const StoryModel = ({setShowModel , fetchStories}) => {
     const bgColors = ["#4f46e5", "#7c3aed", "#1dd1a1", "#feca57", "#ff9ff3", "#54a0ff"];
 
     const [mode , setMode] = useState("text");
@@ -12,7 +14,7 @@ const StoryModel = () => {
     const [media , setMedia] = useState(null);
     const [previewUrl , setPreviewUrl] = useState(null);
 
-    const handleMediaChange = (e) => {
+    const handleMediaUpload = (e) => {
         const file = e.target.files?.[0];
         if (file) {
             setMedia(file);
@@ -26,7 +28,53 @@ const StoryModel = () => {
 
     return (
         <div className='fixed inset-0 z-110 min-h-screen bg-black/80 backdrop-blur text-white flex items-center justify-center p-4'>
+            <div className='w-full max-w-md'>
+                <div className='text-center mb-4 flex items-center justify-between'>
+                    <button onClick={() => setShowModel(false)} className='text-white p-2 cursor-pointer'>
+                        <ArrowLeft />
+                    </button>
+                    <h2 className='text-lg font-semibold'>Create Story</h2>
+                    <span className='w-10'></span>
+                </div>
+                <div className='rounded-lg h-96 flex items-center justify-center relative' style={{ backgroundColor : background}}>
+                        {mode === 'text' && (
+                            <textarea value={text} onChange={(e) => setText(e.target.value)} className='bg-transparent text-white w-full h-fulll p-6 text-lg resize-none focus:outline-none' placeholder='Write your story...' />
+                        )}
 
+                        {
+                        mode === 'media' && previewUrl && (
+                            media?.type.startsWith('image') ? (
+                                <img src={previewUrl} alt="Story Preview" className='object-contain max-h-full' />
+                            ) : (
+                                <video src={previewUrl} className='object-contain max-h-full'  />
+                            )
+                        )}
+                </div>
+
+                <div className='flex mt-4 gap-2'>
+                    {bgColors.map((color) => (
+                        <button key={color} onClick={() => setBackground(color)} className='w-8 h-8 rounded-full ring cursor-pointer' style={{ backgroundColor: color }} />
+                    ))}
+                </div>
+
+                <div className='flex mt-4 gap-2'>
+                    <button onClick={() => {setMode('text'); setMedia(null); setPreviewUrl(null);}} className={`flex-1 flex item-center justify-center gap-2 p-2 rounded cursor-pointer ${mode === 'text' ? 'bg-indigo-600' : 'bg-gray-700'} `}>
+                        <TextIcon size={18} /> Text
+                    </button>
+                    <label className={`flex flex-1 items-center justify-center gap-2 p-2 rounded cursor-pointer ${mode === 'media' ? 'bg-indigo-600' : 'bg-gray-700'}`}>
+                        <input type="file" accept="image/*,video/*" id="media-upload" className='hidden' onChange={(e)=>{handleMediaUpload(e); setMode('media');}} />
+                        <Upload size={18} /> Photo/Video
+                    </label>
+                </div>
+                <button onClick={()=> toast.promise(handleCreateStory(), {
+                    loading: 'Creating story...',
+                    success: 'Story created successfully!',
+                    error: 'Error creating story.'
+                })} className='flex items-center justify-center gap-2 text-white py-3 mt-4 w-full rounded bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition cursor-pointer'>
+                    <Sparkle size={20} /> Create Story
+                </button>
+
+            </div>
         </div>
   )
 }
--- pixora/src/Pages/Connection.jsx
+++ pixora/src/Pages/Connection.jsx
@@ -1,9 +1,91 @@
 import React from 'react'
+import {Users , UserPlus , UserCheck , UserRoundPen , MessageSquare } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+import dummyConnectionData from '../dummyData/dummyConnectionData'
 
 const Connection = () => {
+  const [currentTab, setCurrentTab] = React.useState('Followers');
+  const navigate = useNavigate();
+  const dataArray =[
+    {label: 'Followers', value: dummyConnectionData.followers.length , icon: <Users className='w-4 h-4' />},
+    {label: 'Following', value: dummyConnectionData.following.length, icon: <UserCheck className='w-4 h-4' />},
+    {label: 'Pending', value: dummyConnectionData.pending.length, icon: <UserRoundPen className='w-4 h-4' />},
+    {label: 'Connections', value: dummyConnectionData.requests.length, icon: <UserPlus className='w-4 h-4' />}
+  ]
   return (
-    <div>
-        <h1 className='text-3xl font-bold text-center mt-10'>Connection Page</h1>
+    <div className='min-h-screen bg-slate-50'>
+        <div className='max-w-6xl mx-auto p-6'>
+          <div className='mb-8'>
+            <h1 className='text-3xl font-bold text-slate-800 mb-2'>Connection</h1>
+            <p className='text-slate-600'>Manage your connections and stay in touch with people.</p>
+          </div>
+
+          <div className='mb-8 flex flex-wrap gap-8'>
+            {dataArray.map((item, index) => (
+              <div key={index} className='flex flex-col items-center justify-center gap-1 border h-20 w-40 border-gray-200 bg-white rounded-md shadow'>
+                {item.icon}
+                <div>
+                  <p className='text-sm text-gray-500'>{item.label}</p>
+                  <p className='text-lg font-semibold text-gray-800'>{item.value}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className='inline-flex flex-wrap items-center border border-gray-200 rounded-md p-1 bg-white shadow-sm'>
+            {dataArray.map((tab)=>(
+              <button onClick={() => setCurrentTab(tab.label)} key={tab.label} className={` cursor-pointer flex items-center px-3 py-1 text-sm rounded-md transitions-colors ${currentTab === tab.label ? 'bg-white font-medium text-black' : 'text-gray-600 hover:text-black'}`}>
+                <tab.icon className='w-4 h-4' />
+                <span className='ml-1'>{tab.label}</span>
+                {tab.count !== undefined && <span className='ml-2 text-xs bg-gray-100 text-gray-500 px-2 py-0.5 rounded-full'>({tab.count})</span>}
+              </button>
+            ))}
+          </div>
+
+          {/* Connection Details */}
+          <div className='flex flex-wrap gap-6 mt-6'>
+            {dataArray.find((item) => item.label === currentTab).value.map((user) => (
+              <div key={user._id} className='w-full max-w-88 flex gap-5 p-6 bg-white shadow rounded-md'>
+                <img src={user.profile_picture} alt={user.name} className='rounded-full w-12 h-12 shadow-md mx-auto' />
+                <div className='flex-1'>
+                  <p className='font-medium text-gray-800'>{user.full_name}</p>
+                  <p className='text-gray-500'>@{user.username}</p>
+                  <p className='text-sm text-gray-500'>{user.bio.slice(0 , 30)}...</p>
+                  <p className='text-xs text-gray-400'>Joined {new Date(user.createdAt).toLocaleDateString()}</p>
+                  <div className='flex max-sm:flex-col gap-2 mt-4'>
+                    {
+                      <button onClick={() => navigate(`/profile/${user._id}`)} className='w-full p-2 text-sm rounded bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 active:scale-95 transition text-white cursor-pointer'>
+                        View Profile
+                      </button>
+                    }
+                    {
+                      currentTab === 'Following' && (
+                        <button onClick={() => navigate(`/profile/${user._id}`)} className='w-full p-2 text-sm rounded bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 active:scale-95 transition text-white cursor-pointer'>
+                          Unfollow
+                        </button>
+                      ) 
+                    }
+                    {
+                      currentTab === 'Pending' && (
+                        <button onClick={() => navigate(`/profile/${user._id}`)} className='w-full p-2 text-sm rounded bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 active:scale-95 transition text-white cursor-pointer'>
+                          Accept
+                        </button>
+                      ) 
+                    }
+                    {
+                      currentTab === 'Connections' && (
+                        <button onClick={() => navigate(`/messages/${user._id}`)} className='w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-900 active:scale-95 transition cursor-pointer flex items-center justify-center gap-1'>
+                          <MessageSquare className='w-4 h-4'/>
+                          Message
+                        </button>
+                      ) 
+                    }
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
     </div>
   )
 }
