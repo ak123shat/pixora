93a4c2a2e7c5d20a92057570359e7bd010825723
ak123shat <jainakshat979@gmail.com>
2025-08-14 07:34:02 +0100

    Some error fixed

--- Server/Ingest/index.js
+++ Server/Ingest/index.js
@@ -1,57 +1,59 @@
-import {Inngest} from 'inngest';
-import User from '../models/User.js';
+import { Inngest } from "inngest";
+import User from "../models/User.js";
 
-export const inngest = new Inngest({id : "Pixora-app" });
+export const inngest = new Inngest({ id: "Pixora-app" });
 
-
-
-const syncUser = functions.createUser({
-  name: 'Sync User',
-  description: 'Sync user data with external service',
-  handler: async (event) => {
-    const { id , first_name , last_name , email_address , image_url } = event.data;
-    let username = email_address[0].email_address.split('@')[0];
-
-    const User = await User.findOne({username})
-
-    if(User){
-        username = username + Math.floor(Math.random() * 1000);
-    }
-    const userData = {
-        _id: id,
-        full_name: first_name + ' ' + last_name,
-        email: email_address[0].email_address,
-        profile_picture: image_url,
-        username
-    }
-    await User.create(userData);
-  },
-});
-
-// update user data
-export const updateUser = functions.createUser({
-  name: 'Update User',
-  description: 'Update user data in external service',
-  handler: async (event) => {
+// Sync user data
+export const syncUser = inngest.createFunction(
+  { name: "Sync User", id: "sync-user-fn" }, // Explicit unique ID
+  { event: "user/sync" }, // Event name that will trigger this function
+  async ({ event }) => {
     const { id, first_name, last_name, email_address, image_url } = event.data;
-    const UpdatedUserData = {
-        
-        full_name: first_name + ' ' + last_name,
-        email: email_address[0].email_address,
-        profile_picture: image_url
-    }
-    await User.findByIdAndUpdate(id, UpdatedUserData);
-  },
-});
 
-// delete user data
-export const deleteUser = functions.createUser({
-  name: 'Delete User',
-  description: 'Delete user data in external service',
-  handler: async (event) => {
+    let username = email_address[0].email_address.split("@")[0];
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      username += Math.floor(Math.random() * 1000);
+    }
+
+    const userData = {
+      _id: id,
+      full_name: `${first_name} ${last_name}`,
+      email: email_address[0].email_address,
+      profile_picture: image_url,
+      username,
+    };
+
+    await User.create(userData);
+  }
+);
+
+// Update user data
+export const updateUser = inngest.createFunction(
+  { name: "Update User", id: "update-user-fn" },
+  { event: "user/update" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_address, image_url } = event.data;
+
+    const UpdatedUserData = {
+      full_name: `${first_name} ${last_name}`,
+      email: email_address[0].email_address,
+      profile_picture: image_url,
+    };
+
+    await User.findByIdAndUpdate(id, UpdatedUserData);
+  }
+);
+
+// Delete user data
+export const deleteUser = inngest.createFunction(
+  { name: "Delete User", id: "delete-user-fn" },
+  { event: "user/delete" },
+  async ({ event }) => {
     const { id } = event.data;
     await User.findByIdAndDelete(id);
-  },
-});
+  }
+);
 
-export const functions = [syncUser, updateUser , deleteUser];
+export const functions = [syncUser, updateUser, deleteUser];
